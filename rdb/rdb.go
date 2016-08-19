fec9678c61be965adac881ebb3db15509cd3e3c9
Mikael Cabot <mikaelcabot@gmail.com>
2016-08-19 12:43:32 +0200

    index, table create

--- rdb/rdb.go
+++ rdb/rdb.go
@@ -100,3 +100,57 @@ func TableExists(name string, s *gorethink.Session) (exists bool, err error) {
 	}
 	return
 }
+
+// EnsureTable creates a table if it does not already exist
+func EnsureTable(name string, s *gorethink.Session) (err error) {
+	var exists bool
+	if exists, err = TableExists(name, s); err != nil || exists {
+		return
+	}
+
+	err = gorethink.TableCreate(name).Exec(s)
+	return
+}
+
+// IndexExists checks if a table index exists
+func IndexExists(table gorethink.Term, index string, s *gorethink.Session) (exists bool, err error) {
+	exists = false
+	res, err := table.IndexList().Run(s)
+	if err != nil {
+		return
+	}
+	defer res.Close()
+	var indices []string
+	if err = res.All(&indices); err != nil {
+		return
+	}
+	for _, t := range indices {
+		if t == index {
+			exists = true
+			return
+		}
+	}
+	return
+}
+
+// EnsureIndex creates an index if not already exist
+func EnsureIndex(table gorethink.Term, index string, s *gorethink.Session) (err error) {
+	var exists bool
+	if exists, err = IndexExists(table, index, s); err != nil || exists {
+		return
+	}
+
+	err = table.IndexCreate(index).Exec(s)
+	return
+}
+
+// EnsureIndexFunc creates an index if not already exist
+func EnsureIndexFunc(table gorethink.Term, index string, indexFunction interface{}, s *gorethink.Session) (err error) {
+	var exists bool
+	if exists, err = IndexExists(table, index, s); err != nil || exists {
+		return
+	}
+
+	err = table.IndexCreateFunc(index, indexFunction).Exec(s)
+	return
+}
