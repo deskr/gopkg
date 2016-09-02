ad3fb6762143cb8854147edeb8f7125abcf23064
Mikael Cabot <mikaelcabot@gmail.com>
2016-09-02 10:21:04 +0200

    cleanup rdb utils

--- rdb/rdb.go
+++ rdb/rdb.go
@@ -1,66 +1,39 @@
 package rdb
 
-import (
-	"fmt"
-	"time"
+import "gopkg.in/dancannon/gorethink.v2"
 
-	"gopkg.in/dancannon/gorethink.v2"
-)
-
-var sessions = make(map[string]*gorethink.Session, 0)
-
-// OpenConnection initializes the rethink db
-func OpenConnection(address string, database string) (c *gorethink.Session, err error) {
-	skey := fmt.Sprintf("%s#%s", address, database)
-
-	if s, ok := sessions[skey]; ok && s.IsConnected() {
-		c = s
-		return
-	}
-
+// OpenSession initializes the rethink db session
+func OpenSession(address string, database string) (session *gorethink.Session, err error) {
 	gorethink.SetTags("gorethink", "json")
 
-	for i := 0; i < 10; i++ {
-		c, err = gorethink.Connect(gorethink.ConnectOpts{
-			Address:  address,
-			Database: database,
-			MaxIdle:  10,
-			MaxOpen:  10,
-		})
-		if err == nil {
-			break
-		} else {
-			time.Sleep(time.Second * 2)
-		}
-	}
-
+	session, err = gorethink.Connect(gorethink.ConnectOpts{
+		Address:  address,
+		Database: database,
+	})
 	if err != nil {
 		return
 	}
 
 	// Create database if not already exists
-	exists, err := DatabaseExists(database, c)
+	exists, err := DatabaseExists(database, session)
 	if err != nil {
+		session.Close()
 		return
 	}
 
 	if !exists {
-		err = gorethink.DBCreate(database).Exec(c)
-		if err != nil {
+		if err = gorethink.DBCreate(database).Exec(session); err != nil {
+			session.Close()
 			return
 		}
 	}
 
-	c.Use(database)
-
-	sessions[skey] = c
-
+	session.Use(database)
 	return
 }
 
 // DatabaseExists checks if a database exists
 func DatabaseExists(name string, s *gorethink.Session) (exists bool, err error) {
-	exists = false
 	res, err := gorethink.DBList().Run(s)
 	if err != nil {
 		return
@@ -82,7 +55,6 @@ func DatabaseExists(name string, s *gorethink.Session) (exists bool, err error) 
 
 // TableExists checks if a table exists
 func TableExists(name string, s *gorethink.Session) (exists bool, err error) {
-	exists = false
 	res, err := gorethink.TableList().Run(s)
 	if err != nil {
 		return
@@ -102,19 +74,16 @@ func TableExists(name string, s *gorethink.Session) (exists bool, err error) {
 }
 
 // EnsureTable creates a table if it does not already exist
-func EnsureTable(name string, s *gorethink.Session) (err error) {
-	var exists bool
-	if exists, err = TableExists(name, s); err != nil || exists {
-		return
+func EnsureTable(name string, s *gorethink.Session) error {
+	if exists, err := TableExists(name, s); err != nil || exists {
+		return err
 	}
 
-	err = gorethink.TableCreate(name).Exec(s)
-	return
+	return gorethink.TableCreate(name).Exec(s)
 }
 
 // IndexExists checks if a table index exists
 func IndexExists(table gorethink.Term, index string, s *gorethink.Session) (exists bool, err error) {
-	exists = false
 	res, err := table.IndexList().Run(s)
 	if err != nil {
 		return
@@ -134,23 +103,19 @@ func IndexExists(table gorethink.Term, index string, s *gorethink.Session) (exis
 }
 
 // EnsureIndex creates an index if not already exist
-func EnsureIndex(table gorethink.Term, index string, s *gorethink.Session) (err error) {
-	var exists bool
-	if exists, err = IndexExists(table, index, s); err != nil || exists {
-		return
+func EnsureIndex(table gorethink.Term, index string, s *gorethink.Session) error {
+	if exists, err := IndexExists(table, index, s); err != nil || exists {
+		return err
 	}
 
-	err = table.IndexCreate(index).Exec(s)
-	return
+	return table.IndexCreate(index).Exec(s)
 }
 
 // EnsureIndexFunc creates an index if not already exist
-func EnsureIndexFunc(table gorethink.Term, index string, indexFunction interface{}, s *gorethink.Session) (err error) {
-	var exists bool
-	if exists, err = IndexExists(table, index, s); err != nil || exists {
-		return
+func EnsureIndexFunc(table gorethink.Term, index string, indexFunction interface{}, s *gorethink.Session) error {
+	if exists, err := IndexExists(table, index, s); err != nil || exists {
+		return err
 	}
 
-	err = table.IndexCreateFunc(index, indexFunction).Exec(s)
-	return
+	return table.IndexCreateFunc(index, indexFunction).Exec(s)
 }
